17ebe786c4e8d1546a2fd774c89c0d18aa585e70
Michael Iden <michaeliden90@gmail.com>
2017-11-25 22:24:03 -0500

    Removed legalese from original example

--- src/treat-feeder.ino
+++ src/treat-feeder.ino
@@ -1,19 +1,6 @@
-// Adafruit IO Analog In Example
-// Tutorial Link: https://learn.adafruit.com/adafruit-io-basics-analog-input
-//
-// Adafruit invests time and resources providing this open source code.
-// Please support Adafruit and open source hardware by purchasing
-// products from Adafruit!
-//
-// Written by Todd Treece for Adafruit Industries
-// Copyright (c) 2016 Adafruit Industries
-// Licensed under the MIT license.
-//
-// All text above must be included in any redistribution.
-
 /************************** Configuration ***********************************/
 
-// edit the config.h tab and enter your Adafruit IO credentials
+// Edit the config.h tab and enter your Adafruit IO credentials
 // and any additional configuration needed for WiFi, cellular,
 // or ethernet clients.
 #include "config.h"
